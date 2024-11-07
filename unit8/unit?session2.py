7ca41d25396f65f55322e5cac9aef91537a90a0f
Hama <pashazad@msu.edu>
2024-11-06 19:36:18 -0500

    Add initial structure for unit8 session files with placeholders for understanding and planning

---
+++ unit8/unit?session2.py
@@ -0,0 +1,45 @@
+"""
+Set 1
+
+Understand:
+
+
+Plan:
+
+"""
+# Implement:
+
+"""
+Set 2
+
+Understand:
+
+
+Plan:
+
+"""
+# Implement:
+
+
+"""
+Advanced Set 1
+
+Understand:
+
+
+Plan:
+
+"""
+# Implement:
+
+"""
+Advanced Set 2
+
+Understand:
+
+
+Plan:
+
+"""
+# Implement:
+
